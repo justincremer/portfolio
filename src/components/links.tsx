51b9e229504edb3fe0a5eb46606516800c25fc7c
justincremer <jacremer@live.com>
2021-05-28 19:06:08 -0700

    started links

--- src/components/links.tsx
+++ src/components/links.tsx
@@ -1,7 +1,22 @@
 export const Links: React.FC = (): JSX.Element => {
   return (
-	<div className="m-4">
-	  <header>Insert links here</header>
+	<div className="flex-0">
+	  <a href="https://github.com/justincremer" target="blank">
+		<img
+		  src="https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/github.svg"
+		  alt="justincremer"
+		  height="30"
+		  width="40"
+		/>
+	  </a>
+	  <a href="https://github.com/justincremer" target="blank">
+		<img
+		  src="https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/gmail.svg"
+		  alt="justincremer"
+		  height="30"
+		  width="40"
+		/>
+	  </a>
 	</div>
   );
 };
