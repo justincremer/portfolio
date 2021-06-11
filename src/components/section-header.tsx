4bf4a0e7a61383c3465d946c13f31bfe9cfcc93e
Aaron Marsh <aaronmarsh755@gmail.com>
2021-06-11 00:47:34 -0400

    fixed padding

--- src/components/section-header.tsx
+++ src/components/section-header.tsx
@@ -3,5 +3,5 @@ type Props = {
 };
 
 export const SectionHeader: React.FC<Props> = ({ title }): JSX.Element => {
-  return <header className="mb-4 text-2xl xs:p-5">{title}</header>;
+  return <header className="mb-4 text-2xl p-5 ">{title}</header>;
 };
