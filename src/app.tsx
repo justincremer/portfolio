f17265a857a82e3fed4df3320bcbb428fb277c9a
justincremer <jacremer@live.com>
2021-06-15 18:36:08 -0700

    adds react router

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,11 @@
+import { Route, BrowserRouter as Router } from "react-router-dom";
 import { JustinCremer } from "./pages";
 
-export const App: React.FC = (): JSX.Element => <JustinCremer />;
+export const App: React.FC = (): JSX.Element => {
+  return (
+	<Router>
+	  <Route path="/" />
+	  <Route path="/justincremer" component={JustinCremer} />
+	</Router>
+  );
+};
